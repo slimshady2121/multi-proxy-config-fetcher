b1eb145aab4c0eb00da873447af3add22e0ba240
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-09-06 20:07:46 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -4,7 +4,6 @@
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
     "https://t.me/s/v2rayfree",
-    # "https://t.me/s/FreeV2rays",
     "https://t.me/s/v2ray_free_conf",
     "https://t.me/s/PrivateVPNs",
     "https://t.me/s/IP_CF_Config",
@@ -12,21 +11,13 @@ SOURCE_URLS = [
     "https://t.me/s/meli_proxyy",
     "https://t.me/s/DirectVPN",
     "https://t.me/s/moftconfig",
-    # "https://t.me/s/proxyiranip",
-    # "https://t.me/s/v2nodes",
-    # "https://t.me/s/grizzlyvpn",
     "https://t.me/s/v2ray_extractor",
     "https://t.me/s/v2ray_configs_pool",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/trojan",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/vless",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/vmess",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/shadowsocks",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/hysteria", 
-    "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
+    "https://raw.githubusercontent.com/MahsaNetConfigTopic/config/refs/heads/main/xray_final.txt",
+    "https://raw.githubusercontent.com/imegabiz/ss-config-updater/refs/heads/main/configs.txt",
     "https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt",
     "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt",
-    "https://raw.githubusercontent.com/itsyebekhe/PSG/main/lite/subscriptions/xray/normal/mix",
+    "https://raw.githubusercontent.com/itsyebekhe/PSG/main/subscriptions/xray/base64/mix",
     "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
     # Add more URLs here if you want to include additional sources.
 ]
