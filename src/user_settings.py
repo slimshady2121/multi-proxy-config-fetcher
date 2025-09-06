387e799fbf06c8491c1fa33de7edb10399dff0ab
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-09-06 20:44:34 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -11,7 +11,6 @@ SOURCE_URLS = [
     "https://raw.githubusercontent.com/MahsaNetConfigTopic/config/refs/heads/main/xray_final.txt",
     "https://raw.githubusercontent.com/imegabiz/ss-config-updater/refs/heads/main/configs.txt",
     "https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt",
-    "https://raw.githubusercontent.com/itsyebekhe/PSG/main/subscriptions/xray/base64/mix",
     "https://raw.githubusercontent.com/darkvpnapp/CloudflarePlus/refs/heads/main/proxy",
     "https://v2.alicivil.workers.dev",
     # Add more URLs here if you want to include additional sources.
